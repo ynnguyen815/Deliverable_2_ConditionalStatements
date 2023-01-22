0c77acf6e67d4c6f30735188a1ed870e921e8d6a
alexf <alexf@LAPTOP-261VL15G>
2023-01-22 15:36:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Author: Nhi Nguyen
+Date: 1/22/2023
+Description: C# Console Application for calculating letter grades
+*/
+
+using System;
+
+namespace Deliverable_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numeric grade: ");
+
+            try
+            {
+
+                decimal grade = decimal.Parse(Console.ReadLine());
+
+                string letter;
+
+                if (grade >= 90)
+                {
+                    letter = "A";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is " + letter);
+                }
+
+                else if (grade >= 80 && grade < 90)
+                {
+                    letter = "B";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is " + letter);
+                }
+
+                else if (grade >= 70 && grade < 80)
+                {
+                    letter = "C";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is " + letter);
+                }
+
+                else if (grade >= 60 && grade < 70)
+                {
+                    letter = "D";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is " + letter);
+                }
+
+                else
+                {
+                    letter = "F";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is " + letter);
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Enter a numeric value");
+            }
+        }
+    }
+}
